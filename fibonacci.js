b521b4b774e8ef264481936325c22d74d0161078
lucifer2355 <dgajjar999@gmail.com>
2020-09-07 19:30:22 +0530

    add time complexity of fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,13 @@
 function fibonacci(n) {
-  const number = [1, 1];
+  const number = [1, 1]; //* 1
   for (let i = 2; i < n + 1; i++) {
-    number.push(number[i - 2] + number[i - 1]);
+    //* 1
+    number.push(number[i - 2] + number[i - 1]); //* n - 1
   }
-  return number[n];
+  return number[n]; //* 1
 }
+
+//! T = 1 + 1 + n - 1 + 1 = 2 + n = n
+//! O(n) => Linear Time Complexity
 
 console.log(fibonacci(5));
