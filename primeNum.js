7a598b213cd03bb3ea9f4a40853e625b1dc581a0
lucifer2355 <dgajjar999@gmail.com>
2020-09-07 20:04:42 +0530

    add time complexity  of prime number problem

--- primeNum.js
+++ primeNum.js
@@ -1,10 +1,17 @@
 function isPrime(n) {
   for (let i = 2; i < n; i++) {
+    //* 1
     if (n % i === 0) {
-      return false;
+      //* n
+      return false; //* 1
     }
   }
-  return true;
+  return true; //* 1
 }
+
+//! T = 1 + n - 2 + 1 + 1 = 1 + n = n
+//! Best Case: n = 1 OR n = 2 => O(1)
+//! Average Case: O(n)
+//! Worst Case: n = 10,000 => O(n)
 
 console.log(isPrime(5));
