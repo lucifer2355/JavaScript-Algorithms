0a09127c8c4a8750c06e70c35d4eceffa1626a63
lucifer2355 <dgajjar999@gmail.com>
2020-09-07 20:12:20 +0530

    add improved solution of prime number problem

--- primeNum.js
+++ primeNum.js
@@ -1,5 +1,5 @@
 function isPrime(n) {
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i < Math.sqrt(n); i++) {
     //* 1
     if (n % i === 0) {
       //* n
@@ -9,9 +9,11 @@ function isPrime(n) {
   return true; //* 1
 }
 
+//* Math.sqrt(9) = 3
+
 //! T = 1 + n - 2 + 1 + 1 = 1 + n = n
 //! Best Case: n = 1 OR n = 2 => O(1)
 //! Average Case: O(n)
-//! Worst Case: n = 10,000 => O(n)
+//! Worst Case: n = 10,000 => O(n) (Improved: O(sqrt(n)))
 
-console.log(isPrime(5));
+console.log(isPrime(6));
