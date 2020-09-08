a160d4594d7a9622aa16a47aba5355e084f5a626
lucifer2355 <dgajjar999@gmail.com>
2020-09-08 13:06:57 +0530

    add time complexity of min number problem

--- minNumber.js
+++ minNumber.js
@@ -1,14 +1,18 @@
 function minNum(numbers) {
   let minNumber = numbers[0]; //* 1
-  for (let i = 1; i <= numbers.length; i++) {
+  for (const num of numbers) {
     //* 1
-    if (minNumber > numbers[i]) {
-      //* 1
-      minNumber = numbers[i]; //* n
+    if (minNumber > num) {
+      //* n
+      minNumber = num; //* 0 (Best Case), n - 1 (Worst Case), 1 (Average Case)
     }
   }
   //   return numbers.sort((a, b) => a - b)[0];
   return minNumber; //* 1
 }
+
+//! Best Case: [1, 2, 3] => n => O(n)
+//! Worst Case: [3, 2, 1] => n => O(n)
+//! Average Case: [2, 1, 3] => n => O(n)
 
 console.log(minNum([1, 2, 3]));
