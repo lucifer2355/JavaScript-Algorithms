4c942e8a3d10d0458e4e0976fb4263c47efd9140
lucifer2355 <dgajjar999@gmail.com>
2020-09-08 19:35:10 +0530

    add is power of two check problem

---
+++ isPowerOfTwo.js
@@ -0,0 +1,18 @@
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  let divideNumber = n;
+  while (divideNumber !== 1) {
+    if (divideNumber & (2 !== 0)) {
+      return false;
+    }
+    divideNumber = divideNumber / 2;
+  }
+  return true;
+}
+
+console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(5));
+console.log(isPowerOfTwo(16));
+console.log(isPowerOfTwo(20));
