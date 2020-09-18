0b51142bd433f679c502e72e9e08ac7a7ab54a71
lucifer2355 <dgajjar999@gmail.com>
2020-09-18 23:39:08 +0530

    add quick sort algorithm

---
+++ sorting/quickSort.js
@@ -0,0 +1,33 @@
+function quickSort(arr) {
+  let copiedArray = [...arr];
+
+  if (copiedArray.length <= 1) {
+    return copiedArray;
+  }
+
+  const smallerElementArray = [];
+  const biggerElementArray = [];
+  const pivotElementArray = copiedArray.shift();
+  const centerElementArray = [pivotElementArray];
+
+  while (copiedArray.length) {
+    const currentElement = copiedArray.shift();
+    if (currentElement === pivotElementArray) {
+      centerElementArray.push(currentElement);
+    } else if (currentElement < pivotElementArray) {
+      smallerElementArray.push(currentElement);
+    } else {
+      biggerElementArray.push(currentElement);
+    }
+  }
+
+  const smallerElementSortedArray = quickSort(smallerElementArray);
+  const biggerElementSortedArray = quickSort(biggerElementArray);
+  return smallerElementSortedArray.concat(
+    centerElementArray,
+    biggerElementSortedArray
+  );
+}
+
+const arr = [-3, 10, 1, 100, -10, 22, 15];
+console.log(quickSort(arr));
