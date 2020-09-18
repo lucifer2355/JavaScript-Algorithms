bc0e5a8530c7a53cad6e79bc8a06e5414ce74de3
lucifer2355 <dgajjar999@gmail.com>
2020-09-18 19:22:20 +0530

    add time complexity

--- binarySearch/findElementRecursion.js
+++ binarySearch/findElementRecursion.js
@@ -13,12 +13,19 @@ function findElement(sortedArr, element, offset) {
   } else {
     endIndex = middleIndex;
   }
+
+  //! O(1)
   return findElement(
     sortedArr.slice(startIndex, endIndex + 1),
     element,
     offset
   );
 }
+
+//! a = 1
+//! b = 2
+//! O(n^logb a) => O(n^log2 1) => O(n^0) => O(1)
+//! Overall algorithm time complexity: O(n^logb a * log n) => O(1 * log n) => O(log n)
 
 const arr = [1, 5, 9, 13, 99, 100];
 console.log(findElement(arr, 99, 0));
