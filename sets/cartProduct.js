c5331f1824773e3d0109ff62a272780a6f467a6b
lucifer2355 <dgajjar999@gmail.com>
2020-09-21 19:25:47 +0530

    add time & space complexity of cart product sets problem

--- sets/cartProduct.js
+++ sets/cartProduct.js
@@ -10,7 +10,13 @@ function cartProduct(setA, setB) {
   return product;
 }
 
+//* Time Complexity
+//! T => O(n * m) (n = setA length, m = setB length)
+
+//* Space Complexity
+//! S => O(n * m)
+
 const setA = ["blue", "red"];
-const setB = ["m", "l"];
+const setB = ["s", "m", "l"];
 
 console.log(cartProduct(setA, setB));
