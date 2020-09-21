74375c7b9a31c767d98706d1a41bf8de771c3136
lucifer2355 <dgajjar999@gmail.com>
2020-09-21 19:02:36 +0530

    add space complexity of factorial problem

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -5,9 +5,12 @@ function factorial(n) {
 
   return n * factorial(n - 1);
 }
-
+//* Time Complexity
 //! In every function call => O(1)
 //! But we trigger multiple function calls => n function calls
 //! T => n * O(1) => O(n)
+
+//* Space Complexity
+//! S => O(n)
 
 console.log(factorial(5));
