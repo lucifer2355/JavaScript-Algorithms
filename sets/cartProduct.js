2915fb3ba9306c456bb75cca38c81af2b89352d0
lucifer2355 <dgajjar999@gmail.com>
2020-09-21 22:18:37 +0530

    add cartesian loop of sets

--- sets/cartProduct.js
+++ sets/cartProduct.js
@@ -2,6 +2,10 @@ function cartProduct(setA, setB) {
   const product = [];
 
   for (const setAEl of setA) {
+    // if (!Array.isArray(setAEl)) {
+    //   setAEl = [setAEl];
+    // }
+
     for (const setBEl of setB) {
       product.push([setAEl, setBEl]);
     }
@@ -11,12 +15,25 @@ function cartProduct(setA, setB) {
 }
 
 //* Time Complexity
-//! T => O(n * m) (n = setA length, m = setB length)
+//! T => O(n^x) (x = no. of input array)
 
 //* Space Complexity
-//! S => O(n * m)
+//! S => O(n^x) (x = no. of input array)
 
-const setA = ["blue", "red"];
-const setB = ["s", "m", "l"];
+function cartesian(...sets) {
+  let tempProduct = sets[0];
 
-console.log(cartProduct(setA, setB));
+  for (let i = 1; i < sets.length; i++) {
+    tempProduct = cartProduct(tempProduct, sets[i]);
+  }
+
+  return tempProduct;
+}
+
+const colors = ["blue", "red"];
+const size = ["s", "m", "l"];
+const styles = ["round neck", "v neck"];
+
+// console.log(cartProduct(colors, size));
+console.log(cartesian(colors, size, styles));
+// cartesian(colors, size, styles);
