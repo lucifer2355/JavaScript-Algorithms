8b56eeb997c942ec3f1302be98fbd19f8cfcc8cd
“lucifer2355” <dgajjar999@gmail.com>
2021-01-13 23:11:06 +0530

    fix some code

--- array/cyclicallyRotate.js
+++ array/cyclicallyRotate.js
@@ -1,12 +1,9 @@
 const cyclicallyRotate = (array) => {
-  let last = array[array.length - 1];
-
   for (let i = array.length - 2; i >= 0; i--) {
     let temp = array[i];
     array[i] = array[i + 1];
     array[i + 1] = temp;
   }
-  array[0] = last;
 
   return array;
 };
