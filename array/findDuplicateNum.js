a08843f1af074a5c5de5aec1de26b452ef423258
“lucifer2355” <dgajjar999@gmail.com>
2021-01-14 22:57:25 +0530

    add totoAsMethod for finding duplicate number

--- array/findDuplicateNum.js
+++ array/findDuplicateNum.js
@@ -1,4 +1,3 @@
-//! Try with linked list
 const findDuplicate = (array) => {
   let nums = {};
 
@@ -8,4 +7,22 @@ const findDuplicate = (array) => {
   }
 };
 
-console.log(findDuplicate([1, 2, 3, 4, 4, 5]));
+const totoAsMethod = (array) => {
+  let slow = array[0];
+  let fast = array[0];
+
+  do {
+    slow = array[slow];
+    fast = array[array[fast]];
+  } while (slow !== fast);
+
+  fast = array[0];
+  while (slow !== fast) {
+    slow = array[slow];
+    fast = array[fast];
+  }
+
+  return slow;
+};
+
+console.log(totoAsMethod([2, 5, 9, 6, 9, 3, 8, 9, 7, 1]));
