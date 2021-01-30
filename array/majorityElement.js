122f369a1b4180973eb84d50ff4429801c362672
“lucifer2355” <dgajjar999@gmail.com>
2021-01-30 23:02:10 +0530

    fix one issue in majority element algorithem

--- array/majorityElement.js
+++ array/majorityElement.js
@@ -1,15 +1,19 @@
 const majorityElement = (array) => {
   const n = {};
   let majorElement = 0;
+  let result;
 
   for (const num of array) {
     if (n[num]) {
       n[num]++;
-      if (n[num] > majorElement) majorElement = num;
+      if (n[num] > majorElement) {
+        majorElement = n[num];
+        result = num;
+      }
     } else n[num] = 1;
   }
 
-  return majorElement;
+  return result;
 };
 
 console.log(majorityElement([7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 5, 5, 5, 5]));
