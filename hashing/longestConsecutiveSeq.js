06ecfb9c13b1fbf13f1bb9902df30d82973f3102
“lucifer2355” <dgajjar999@gmail.com>
2021-02-10 23:27:08 +0530

    add longest seq algorithm with second approach

--- hashing/longestConsecutiveSeq.js
+++ hashing/longestConsecutiveSeq.js
@@ -1,4 +1,4 @@
-const longestSeq = (array) => {
+const longestSeq1 = (array) => {
   let result;
   let largestRange = 0;
   const nums = {};
@@ -37,6 +37,37 @@ const longestSeq = (array) => {
   }
 
   return result;
+
+  //! T => O(n) + O(n) = O(2n)
+  //! S => O(n)
 };
 
-console.log(longestSeq([102, 4, 100, 1, 101, 3, 2]));
+const longestSeq2 = (array) => {
+  let longestLength = 0;
+  const nums = {};
+
+  for (const num of array) {
+    nums[num] = true;
+  }
+
+  for (const num of array) {
+    if (nums[num - 1] === undefined) {
+      let currentLength = 1;
+      let currentNum = num;
+
+      while (nums[currentNum + 1]) {
+        currentNum += 1;
+        currentLength++;
+      }
+
+      longestLength = Math.max(longestLength, currentLength);
+    }
+  }
+
+  return longestLength;
+
+  //! T => O(n) + O(n) = O(2n)
+  //! S => O(n)
+};
+
+console.log(longestSeq2([102, 4, 100, 1, 101, 3, 2]));
