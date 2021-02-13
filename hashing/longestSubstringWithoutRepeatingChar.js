34781b065946554d87e9914f3e5929e24983d991
“lucifer2355” <dgajjar999@gmail.com>
2021-02-13 22:35:42 +0530

    add gitignore file

---
+++ hashing/longestSubstringWithoutRepeatingChar.js
@@ -0,0 +1,7 @@
+//! longest substring without repeating characters
+
+const longestSubString = (string) => {
+  //! 1.naive brute-force approach; T = O(n ^ 2); S = O(n)
+};
+
+console.log(longestSubString("abcaabcdba"));
