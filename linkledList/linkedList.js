d00a8d873e49d1f452cd7fd81d2aa97e5844949f
“lucifer2355” <dgajjar999@gmail.com>
2021-03-02 22:26:11 +0530

    add linkee list class

---
+++ linkledList/linkedList.js
@@ -0,0 +1,14 @@
+export class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = { value: value, next: null };
+
+    if (this.tail) this.tail.next = newNode;
+    this.tail = newNode;
+    if (!this.head) this.head = newNode;
+  }
+}
--- linkledList/reverseLinkedList.js
+++ linkledList/reverseLinkedList.js
@@ -1,7 +1,22 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = { value: value, next: null };
+
+    if (this.tail) this.tail.next = newNode;
+    this.tail = newNode;
+    if (!this.head) this.head = newNode;
+  }
+}
+
 const reverseLinkedList = head => {
   let prev = null;
   let curr = head;
-  let next = curr.next;
+  let nextTemp = null;
 
   while (curr != null) {
     nextTemp = curr.next;
@@ -13,4 +28,11 @@ const reverseLinkedList = head => {
   return prev;
 };
 
-console.log(reverseLinkedList());
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+linkedList.append(5);
+
+console.log(reverseLinkedList(linkedList));
