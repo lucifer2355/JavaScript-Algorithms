5d93476251539f6e2e274a4ad5b7b8c51a31f989
“lucifer2355” <dgajjar999@gmail.com>
2021-07-07 16:52:52 +0530

    add reversString problem using recursion

---
+++ recursion/reverseString.js
@@ -0,0 +1,10 @@
+const reverseString = (s, sIndex = 0, eIndex = 0) => {
+  if (eIndex === 0) eIndex = s.length - 1;
+  if (sIndex > eIndex) return s;
+
+  [s[sIndex], s[eIndex]] = [s[eIndex], s[sIndex]];
+
+  return reverseString(s, sIndex + 1, eIndex - 1);
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
