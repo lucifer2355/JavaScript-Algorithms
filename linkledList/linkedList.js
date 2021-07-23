f73abaeea4bfc91761347e1a0d2eda2f4746ba72
“lucifer2355” <dgajjar999@gmail.com>
2021-07-23 18:23:52 +0530

    add prepend function for linkedlist

--- linkledList/linkedList.js
+++ linkledList/linkedList.js
@@ -1,7 +1,7 @@
-export class LinkedList {
+class LinkedList {
   constructor() {
-    this.head = null; //! First element of the list
-    this.tail = null; //! Last element of the list
+    this.head = null;
+    this.tail = null;
   }
 
   append(value) {
@@ -11,4 +11,30 @@ export class LinkedList {
     this.tail = newNode;
     if (!this.head) this.head = newNode;
   }
+
+  prepend(value) {
+    const newNode = { value: value, next: this.head };
+
+    this.head = newNode;
+    if (!this.tail) this.tail = newNode;
+  }
+
+  toArray() {
+    const elements = [];
+
+    let curNode = this.head;
+    while (curNode) {
+      elements.push(curNode);
+      curNode = curNode.next;
+    }
+
+    return elements;
+  }
 }
+
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.prepend(3);
+
+console.log(linkedList.toArray());
