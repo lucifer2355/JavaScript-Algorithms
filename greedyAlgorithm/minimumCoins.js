86682f03a3d147109a6032cf7c47134a9ef31e88
“lucifer2355” <dgajjar999@gmail.com>
2021-08-19 12:52:06 +0530

    add minimum coins problem

--- greedyAlgorithm/minimumCoins.js
+++ greedyAlgorithm/minimumCoins.js
@@ -1,17 +1,21 @@
-const minimumCoins = (array) => {
+const minimumCoins = (coins, target) => {
   const result = [];
-  let findAmount = 87;
+  let i = coins.length - 1;
 
-  for (let i = array.length - 1; i > 0; i--) {
-    if (findAmount === 0) return result;
-    else if (findAmount === array[i]) {
-      result.push(array[i]);
-      return result;
-    } else if (findAmount > array[i]) {
-      result.push(array[i]);
-      findAmount = findAmount - array[i];
-    }
-  }
+  while (i >= 0) {
+    if (target === coins[i]) {
+      result.push(coins[i]);
+      break;
+    } else if (target > coins[i]) {
+      target -= coins[i];
+      result.push(coins[i]);
+    } else i--;
+  } //! O(n)
+
+  return result;
 };
 
-console.log(minimumCoins([1, 2, 5, 10, 20, 50, 100, 500, 1000]));
+//! T => O(n)
+//! S => O(1)
+
+console.log(minimumCoins([1, 2, 5, 10, 20, 50, 100, 500, 1000], 49));
