83b95cef47f1c3416d5078df2c8e9f2713a67d3f
“lucifer2355” <dgajjar999@gmail.com>
2021-08-20 19:50:54 +0530

    add sub set sums problem

--- recursion/subsetSums.js
+++ recursion/subsetSums.js
@@ -1,1 +1,29 @@
-const subsetSumsBruteForce = (array) => {};
+//! 1st. Brute Force
+
+function func(index, sum, arr, n, sumSubset) {
+  if (index === n) {
+    sumSubset.push(sum);
+    return;
+  }
+
+  //! pick the element
+  func(index + 1, sum + arr[index], arr, n, sumSubset);
+
+  //! Do-not pick the element
+  func(index + 1, sum, arr, n, sumSubset);
+}
+
+const subSetSums = (array, n) => {
+  const sumSubset = [];
+  func(0, 0, array, n, sumSubset);
+  sumSubset.sort((a, b) => {
+    if (a < b) return -1;
+  });
+
+  return sumSubset;
+};
+
+//! T => O(2^n)
+//! S => O(2^n log 2^n)
+
+console.log(subSetSums([3, 1, 2], 3));
