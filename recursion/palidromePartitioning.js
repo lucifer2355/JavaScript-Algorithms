21572301aed8be9716ec64736d5864476e380f37
“lucifer2355” <dgajjar999@gmail.com>
2021-09-13 15:18:11 +0530

    add palindrome partitioning problem

--- recursion/palidromePartitioning.js
+++ recursion/palidromePartitioning.js
@@ -1,3 +1,37 @@
 //* Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s. *//
 
-//* A palindrome string is a string that reads the same backward as forward. *//
+//! 1st approach brute force
+
+const palindromePartitioning = (string) => {
+  const result = [];
+  findPalindromePartitioning(0, [], string, result);
+
+  return result;
+};
+
+function findPalindromePartitioning(index, ds, string, result) {
+  if (index === string.length) {
+    result.push([...ds]);
+    return;
+  }
+
+  for (let i = index; i < string.length; ++i) {
+    if (isPalindrome(string, index, i)) {
+      ds.push(string.substring(index, i + 1));
+      findPalindromePartitioning(i + 1, ds, string, result);
+      ds.pop();
+    }
+  }
+}
+
+function isPalindrome(string, start, end) {
+  while (start <= end) {
+    if (string[start++] !== string[end--]) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+console.log(palindromePartitioning("aabb"));
